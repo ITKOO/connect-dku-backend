73e44df69ba6e5390c59837f61c299bfb2b24204
ITKOO <itkoo2000@gmail.com>
2023-06-07 22:51:47 +0900

    setting(main): CORS 설정

--- src/main/java/kr/itkoo/connectdkubackend/ConnectDKuApplication.java
+++ src/main/java/kr/itkoo/connectdkubackend/ConnectDKuApplication.java
@@ -2,12 +2,25 @@ package kr.itkoo.connectdkubackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ConnectDKuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConnectDKuApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
